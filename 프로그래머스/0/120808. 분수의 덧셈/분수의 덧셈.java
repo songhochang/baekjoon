af15a121c630e32d09226ccc03120fa2afd8e54a
songhochang <thdghckd111@naver.com>
2024-07-23 15:58:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 4.11 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,26 +1,62 @@
-
-
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int nsum = 0;
+        int dsum = 0;
+
         int[] answer = new int[2];
+
+        if(denom1 > denom2){
+            if(denom1 % denom2 == 0){
+                nsum = numer1 + numer2*(denom1 / denom2);
+                dsum = denom1;
+            }
+            else{
+                nsum = numer1*denom2 + numer2*denom1;
+                dsum = denom1*denom2;
+            }
+        }
+        else if(denom1 < denom2){
+            if(denom2 % denom1 == 0){
+                nsum = numer2 + numer1*(denom2 / denom1);
+                dsum = denom2;
+            }
+            else{
+                nsum = numer1*denom2 + numer2*denom1;
+                dsum = denom1*denom2;
+            }
+        }
+        else{
+            nsum = numer1 + numer2;
+            dsum = denom1;
+        }
         
-        
-        int mom = denom1*denom2;
-        int son = (denom1*numer2)  + (denom2*numer1);
-        int gcd = getGcd(son, mom);
-        
-        
-        answer[0] = son/gcd;
-        answer[1] = mom/gcd;
-        
-        
+        int max = 0;
+
+        if(nsum == dsum){
+            nsum = 1;
+            dsum = 1;
+        }
+        else if(nsum > dsum){
+            for(int i = 1; i <= dsum; i++){
+                if(nsum % i == 0 && dsum % i == 0){
+                    max = i;
+                }
+            }
+            nsum = nsum / max;
+            dsum = dsum / max;
+        }
+        else{
+            for(int i = 1; i <= nsum; i++){
+                if(nsum % i == 0 && dsum % i == 0){
+                    max = i;
+                }
+            }
+            nsum = nsum / max;
+            dsum = dsum / max;
+        }
+
+        answer[0] = nsum;
+        answer[1] = dsum;
         return answer;
     }
-    public int getGcd(int a, int b) {
-            
-            while(a%b == 0) {
-                return b;
-            }
-            return getGcd(b, a%b);
-        }
 }
