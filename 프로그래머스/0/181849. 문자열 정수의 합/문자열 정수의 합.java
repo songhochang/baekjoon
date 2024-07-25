2f01e748b2002bb46d609d57bcb2ebc54355d3c6
songhochang <thdghckd111@naver.com>
2024-07-25 15:40:49 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        
+        char[] c = num_str.toCharArray();
+
+        for(int i = 0; i < c.length; i++){
+            if(c[i] == '0'){
+                answer = answer + 0;
+            }
+            else if(c[i] == '1'){
+                answer = answer + 1;
+            }
+            else if(c[i] == '2'){
+                answer = answer + 2;
+            }
+            else if(c[i] == '3'){
+                answer = answer + 3;
+            }
+            else if(c[i] == '4'){
+                answer = answer + 4;
+            }
+            else if(c[i] == '5'){
+                answer = answer + 5;
+            }
+            else if(c[i] == '6'){
+                answer = answer + 6;
+            }
+            else if(c[i] == '7'){
+                answer = answer + 7;
+            }
+            else if(c[i] == '8'){
+                answer = answer + 8;
+            }
+            else if(c[i] == '9'){
+                answer = answer + 9;
+            }
+        }
+        
+        return answer;
+    }
+}
